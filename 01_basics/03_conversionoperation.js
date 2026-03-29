b678de45555d6111d036b2fe7adeafb7c919bf74
Nikhil Kulkarni <student.nikhilkulkarni@gmail.com>
2026-03-29 10:40:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//conversion gives
+//"33" => 33
+//"33abc" => it gives NaN but its a number
+// true =>1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn =Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// if 1 => true; 0=> false
+//"" =>false
+//"hello" => true
+
+let num = 33
+
+let stringnum =String(num)
+console.log(stringnum);
+console.log(typeof stringnum);
+
