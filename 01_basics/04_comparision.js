8d3e6167a9f0db508f8e5c53c20571b5cc9e3df7
Nikhil Kulkarni <student.nikhilkulkarni@gmail.com>
2026-03-30 12:34:51 +0000

    comparision operation of string , number, null and undefined/

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+//console.log("2" > 1); //true
+//console.log("02" > 1); //true
+
+
+//console.log(null > 0); //false
+//console.log(null == 0);//the reason is that == and >
+//  works differently
+//console.log(null >= 0); //here >= converts null to zero // true
+
+ 
+//console.log(undefined == 0); //false
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2); // false
