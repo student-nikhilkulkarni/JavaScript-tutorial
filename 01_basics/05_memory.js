c4f2facd792c2f0d3e7b0a1fd0919d9d7484c631
Nikhil Kulkarni <student.nikhilkulkarni@gmail.com>
2026-03-31 17:23:06 +0000

    memory types stack and heap

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,26 @@
+// Stack(primitive), Heap(Non-primitive)
+
+//stack (we get value )
+let myname = "NikhilKulkarni"
+
+let anothername = myname
+anothername = "PramodKumar"
+
+console.log(myname);//nikhilkulkarni
+console.log(anothername);//pramodkumar  as value change here 
+// in one storage only
+
+//heap(here we get reference)
+let user1 = {
+    email: "user1@google.com",
+    upi: "user1@ybl"
+
+}
+
+let user2 = user1
+
+user2.email = "nikhil@google.com"
+
+console.log(user1);//nikhil@google.com (both changes as
+//  here reference is used)
+console.log(user2);//nikhil@google.com
